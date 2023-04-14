a9f88a104843d8fb21032e9cd2695d5220141a23
Shaurya <shauryasood293@gmail.com>
2023-04-14 21:33:01 +0530

    project weather

---
+++ app.js
@@ -0,0 +1,70 @@
+const express=require("express");
+const https=require("https");
+
+const app=express();
+
+
+app.use(express.urlencoded());
+
+const info=require(__dirname+"/info.txt");
+console.log(info);
+
+app.get("/",function(req,res){
+
+  // const url="https://api.openweathermap.org/data/2.5/weather?unit=metric&appid=bf600bfb91fc7ee9a3cfc2b776a98a09&q=London";
+  // https.get(url,function(response){
+  //
+  //   response.on("data",function(data){
+  //     const WeatherData=JSON.parse(data);
+  //
+  //     const temp=WeatherData.main.temp;
+  //     const weatherDescription=WeatherData.weather[0].description;
+  //     const icon=WeatherData.weather[0].icon;
+  //     const imgurl="https://openweathermap.org/img/wn/"+ icon +"@2x.png";
+  //
+  //     console.log(temp);
+  //     console.log(weatherDescription);
+  //
+  //     res.write("<h1>The weather in London is "+ temp +" degree celcius</h1>");
+  //     res.write("<h1>The current weather is " +weatherDescription+"</h1>");
+  //     res.write("<img src="+imgurl+">");
+  //     res.send();
+
+
+      res.sendFile(__dirname + "/index.html");
+
+
+    });
+//   });
+// });
+
+app.post("/",function(req,res){
+  let country=req.body.country;
+  const url=info+country;
+  https.get(url,function(response){
+
+    response.on("data",function(data){
+      const WeatherData=JSON.parse(data);
+
+      const temp=WeatherData.main.temp;
+      const weatherDescription=WeatherData.weather[0].description;
+      const icon=WeatherData.weather[0].icon;
+      const imgurl="https://openweathermap.org/img/wn/"+ icon +"@2x.png";
+
+      console.log(temp);
+      console.log(weatherDescription);
+
+      res.write("<h1>The weather in "+country+" is "+ temp +" degree celcius</h1>");
+      res.write("<h1>The current weather is " +weatherDescription+"</h1>");
+      res.write("<img src="+imgurl+">");
+
+      res.send();
+
+    });
+  });
+
+});
+
+app.listen(3000,function(){
+  console.log("server is running");
+});
